54b24e778dcfdc3b7b2cad66f89608877456a322
Oseni03 <oseniayomide57@gmail.com>
2025-03-31 03:02:39 +0100

    persist the theme selection across page refreshes using localStorage

--- src/providers.tsx
+++ src/providers.tsx
@@ -9,6 +9,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
 			attribute="class"
 			defaultTheme="system"
 			enableSystem
+			storageKey="theme"
 			{...props}
 		>
 			{children}
