57e4cdce5eceffdcc9d191929b9fed2426974618
Oseni03 <oseniayomide57@gmail.com>
2025-05-11 04:35:19 +0100

    modify slug generation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,22 +1,20 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import slugify from "@sindresorhus/slugify";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
 export function generateSlug(text: string): string {
-	return text
-		.toLowerCase()
-		.replace(/[^a-z0-9]+/g, "-")
-		.replace(/(^-|-$)/g, "");
+	return slugify(text);
 }
 
 export async function generateUniqueSlug(
 	text: string,
 	checkUnique: (slug: string) => Promise<boolean>
 ): Promise<string> {
-	let slug = generateSlug(text);
+	let slug = slugify(text);
 	let counter = 1;
 	let uniqueSlug = slug;
 
